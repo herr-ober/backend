5b9eab387930bb62cbe3943ebcfdec21f17c1278
Dennis Hilgert <d.hilgert@laserhub.com>
2023-03-14 17:04:27 +0100

    Add initial code structure

---
+++ src/modules/_sample/interfaces/ISampleService.ts
@@ -0,0 +1,12 @@
+import { ISampleCreateData, ISample, ISampleUpdateData } from '../types'
+
+export interface ISampleService {
+  createSample(data: ISampleCreateData): Promise<ISample>
+  getSampleByUuid(uuid: string): Promise<ISample | null>
+  getSamplesByName(name: string): Promise<ISample[]>
+  updateSampleByUuid(
+    uuid: string,
+    data: ISampleUpdateData
+  ): Promise<Array<number>>
+  deleteSampleByUuid(uuid: string, suppressError?: boolean): Promise<number>
+}
